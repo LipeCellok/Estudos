3039988d0417903b8026548e0d1cfb47ed3617a1
Felipe Espíndola <felipenipponspa@hotmail.com>
2024-09-19 15:40:31 -0300

    Up

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -8,12 +8,24 @@ int total = roll1 + roll2 + roll3;
 
 Console.WriteLine($"Dice roll: {roll1} {roll2} {roll3}");
 
-if (total > 14)
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("Você Venceu!!");
+    Console.WriteLine("2 dados iguais! +2 bonus total!!");
+    total +=2;
+}
+
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    Console.WriteLine("3 dados iguais!! + 6 de bonus total!");
+    total +=6;
+}
+
+if (total >= 15)
+{
+    Console.WriteLine($"Você Venceu!! seu total:{total}");
 }
 
 if (total <15)
 {
-    Console.Write("Desculpe, Você perdeu.");
+    Console.Write($"Desculpe, Você perdeu. seu total:{total}");
 }
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c003924016542fa9538f405c7a7805c2dbf64158")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
